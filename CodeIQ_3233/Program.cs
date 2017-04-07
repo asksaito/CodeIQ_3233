9ee3ffc96fbe5c3a6c7682e49e0c7963076217df
ask saito <es.engineer79@gmail.com>
2017-04-08 00:58:35 +0900

    完成版

--- CodeIQ_3233/Program.cs
+++ CodeIQ_3233/Program.cs
@@ -23,11 +23,11 @@ namespace CodeIQ_3233
             //string[] input = { "4", "10" }; // 84
             //string[] input = { "5", "20" }; // 2826
             //string[] input = { "10", "30" }; // 8337880
-            string[] input = { "40", "49" }; // 1677106600
+            //string[] input = { "40", "49" }; // 1677106600
             //string[] input = { "9", "18" }; // 24301
 
             // 標準入力からインプットを取得
-            //string[] input = Console.ReadLine().Split(' ');
+            string[] input = Console.ReadLine().Split(' ');
 
             int count = int.Parse(input[0]);    // m
             int distance = int.Parse(input[1]); // n
@@ -42,46 +42,6 @@ namespace CodeIQ_3233
             // 標準出力に結果出力
             Console.WriteLine(result.ToString());
         }
-
-        //private static int CalcLockPattern(int remainCount, int distance)
-        //{
-        //    int patternCount = 0;
-
-        //    //System.Diagnostics.Debug.Assert(remainCount <= distance);
-
-        //    if (remainCount == distance)
-        //    {
-        //        // ダイヤル回転回数と総移動距離が同じ場合、必ず1パターンに定まる
-        //        return 1;
-        //    }
-
-        //    // 最小移動距離
-        //    int lowestMoveCount = distance - ((remainCount - 1) * 9);
-        //    if (lowestMoveCount <= 1)
-        //    {
-        //        lowestMoveCount = 1;
-        //    }
-
-        //    // 最大移動距離
-        //    int highestMoveCount = distance - remainCount + 1;
-        //    if (highestMoveCount > 9)
-        //    {
-        //        highestMoveCount = 9;
-        //    }
-
-        //    if (remainCount <= 2)
-        //    {
-        //        return highestMoveCount - lowestMoveCount + 1;
-        //    }
-
-        //    for (int moveCount = lowestMoveCount; moveCount <= highestMoveCount; moveCount++)
-        //    {
-        //        // 再帰呼び出し
-        //        patternCount += CalcLockPattern(remainCount - 1, distance - moveCount);
-        //    }
-
-        //    return patternCount;
-        //}
 
         /// <summary>
         /// パターン数を計算する（再帰処理）
@@ -108,31 +68,7 @@ namespace CodeIQ_3233
                 }
             }
             else
-            {
-                //// 最小移動距離を計算
-                //int lowestMoveCount = distance - ((count - 1) * 9);
-                //if (lowestMoveCount <= 1)
-                //{
-                //    lowestMoveCount = 1;
-                //}
-
-                //if (idx > 0)
-                //{
-                //    if (patternTemp[idx - 1] > lowestMoveCount)
-                //    {
-                //        // パターン領域のひとつ前の値より大きい値のみを対象とする
-                //        lowestMoveCount = patternTemp[idx - 1];
-                //    }
-                //}
-
-                //// 最大移動距離を計算
-                //int highestMoveCount = distance - count + 1;
-                //if (highestMoveCount > 9)
-                //{
-                //    // 9が最大値
-                //    highestMoveCount = 9;
-                //}
-                
+            {   
                 int lowestNum = 1;    // 組み合わせ候補の最小値
                 int highestNum = 9;   // 組み合わせ候補の最大値
                 if (idx > 0)
@@ -174,66 +110,26 @@ namespace CodeIQ_3233
         /// <returns>組み合わせ数</returns>
         private static long CalcPermutation(int[] pattern)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int num3 = 0;
-            int num4 = 0;
-            int num5 = 0;
-            int num6 = 0;
-            int num7 = 0;
-            int num8 = 0;
-            int num9 = 0;
-
+            int[] overlapCount = new int[9];
             foreach (int p in pattern)
             {
-                switch (p)
-                {
-                    case 1:
-                        num1++;
-                        break;
-                    case 2:
-                        num2++;
-                        break;
-                    case 3:
-                        num3++;
-                        break;
-                    case 4:
-                        num4++;
-                        break;
-                    case 5:
-                        num5++;
-                        break;
-                    case 6:
-                        num6++;
-                        break;
-                    case 7:
-                        num7++;
-                        break;
-                    case 8:
-                        num8++;
-                        break;
-                    case 9:
-                        num9++;
-                        break;
-                    default:
-                        System.Diagnostics.Debug.Assert(false);
-                        break;
-                }
+                // 各数字の重複個数を調べる
+                overlapCount[p - 1]++;
             }
 
             // 階乗の計算時間を抑えるために、あらかじめ一番重複が多いものを選んでおく（ソートの結果の先頭が一番大きな重複数）
-            List<int> sort = new List<int>() { num1, num2, num3, num4, num5, num6, num7, num8, num9 };
-            sort.Sort();
-            sort.Reverse();
+            List<int> overlap = new List<int>(overlapCount);
+            overlap.Sort();
+            overlap.Reverse();
             
-            int factorialCalcCount = pattern.Count() - sort[0]; // 階乗の計算量を抑えるための工夫
+            int factorialCalcCount = pattern.Count() - overlap[0]; // 階乗の計算量を抑えるための工夫（一番重複個数が多いもので減算）
 
             // 重複順列の計算！ （n! / r1! + r2! ... r9!）
             long permutationResult = Factorial(pattern.Count(), factorialCalcCount);
-            for (int i = 1; i < sort.Count; i++)
+            for (int i = 1; i < overlap.Count; i++)
             {
                 // 重複分をさらに除去していく
-                permutationResult /= Factorial(sort[i], sort[i]);
+                permutationResult /= Factorial(overlap[i], overlap[i]);
             }
 
             // 結果
